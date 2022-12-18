b3a31d3be2143b5b7ade857604d2b9359dc29662
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-12-18 12:04:08 -0300

    creation of the base architecture the project

---
+++ src/Web/MVC/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NSE.WebApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
