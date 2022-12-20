3410f86e7afd3234c49dd7cfdb6974d3093ced8f
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-12-20 14:19:31 -0300

    program com start up

--- src/Services/Identidade/NSE.Identidade.API/Program.cs
+++ src/Services/Identidade/NSE.Identidade.API/Program.cs
@@ -1,25 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
+using NSE.Identidade.API;
+using NSE.Identidade.API.Extensions;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+var builder = WebApplication.CreateBuilder(args)
+                .UseStartup<Startup>();
