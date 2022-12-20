6c3fe0d890db6f9d1e22bcaa00b03fd545dd21d6
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-12-20 14:19:59 -0300

    configuração do contexto do bd com identity

---
+++ src/Services/Identidade/NSE.Identidade.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Identidade.API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
