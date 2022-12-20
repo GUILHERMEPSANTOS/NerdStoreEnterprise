b3f4802c4ea471facd54a13d59d9741483a1164c
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-12-20 14:18:07 -0300

    feat: controller de autenticação e registro de usuarios

---
+++ src/Services/Identidade/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/identidade")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registrar([FromBody] UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new IdentityUser()
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true,
+            };
+
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<IActionResult> Login([FromBody] UsuarioLogin usuarioLogin)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, isPersistent: false, lockoutOnFailure: true);
+
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        
+    }
+}
