6651655c6b11b93cd22d37629bda0f91defc6484
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-12-20 16:13:20 -0300

    objeto com as claims do usuário

---
+++ src/Services/Identidade/NSE.Identidade.API/Models/UserClaim.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Models
+{
+    public class UserClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
