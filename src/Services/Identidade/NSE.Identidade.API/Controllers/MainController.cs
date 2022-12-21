8c792049f8c1c86bc67d63888a5ce70f585238ce
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-12-21 16:30:46 -0300

    feat: controller para tratamento de erros

---
+++ src/Services/Identidade/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+
+        protected ICollection<string> Errors = new List<string>();
+
+
+        protected IActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Message", Errors.ToArray()}
+            }));
+        }
+
+        protected IActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var error in errors)
+            {
+                AddErrorsProcessing(error.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+
+        protected bool ValidOperation()
+        {
+            return !Errors.Any();
+        }
+
+        protected void AddErrorsProcessing(string error)
+        {
+            Errors.Add(error);
+        }
+
+        protected void ClearErrors()
+        {
+            Errors.Clear();
+        }
+    }
+}
