b51b740300747979e2c7dd7075b060e0853941f4
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-03 17:47:59 -0300

    refactor: implementado melhorias no Http Service

--- src/Web/MVC/NSE.WebApp.MVC/Services/AuthenticationService.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Services/AuthenticationService.cs
@@ -1,6 +1,6 @@
-using System.Text;
-using System.Text.Json;
+using Microsoft.Extensions.Options;
 using NSE.WebApp.MVC.Authentication;
+using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Interfaces;
 using NSE.WebApp.MVC.Models;
 using NSE.WebApp.MVC.Models.Errors;
@@ -11,41 +11,43 @@ namespace NSE.WebApp.MVC.Services
     {
         private readonly HttpClient _httpClient;
 
-        public AuthenticationService(HttpClient httpClient)
+        public AuthenticationService(HttpClient httpClient, IOptions<AppSettings> settings)
         {
+
             _httpClient = httpClient;
+             _httpClient.BaseAddress = new Uri(settings.Value.AuthenticationUrl);
         }
 
         public async Task<UserLoginResponse> Login(LoginViewModel loginViewModel)
         {
 
-            var response = await _httpClient.PostAsJsonAsync("http://localhost:5140/api/identidade/autenticar", loginViewModel);
+            var response = await _httpClient.PostAsJsonAsync("/api/identidade/autenticar", loginViewModel);
 
             if (!HandleResponseError(response))
             {
                 return await ReturnResponseError(response);
             }
 
-            return await SerializeResponse<UserLoginResponse>(response);
+            return await DeserializeResponse<UserLoginResponse>(response);
         }
 
         public async Task<UserLoginResponse> Register(RegisterViewModel registerViewModel)
         {
-            var response = await _httpClient.PostAsJsonAsync("http://localhost:5140​/api​/identidade​/nova-conta", registerViewModel);
+            var response = await _httpClient.PostAsJsonAsync("/api/identidade/nova-conta", registerViewModel);
 
             if (!HandleResponseError(response))
             {
                 return await ReturnResponseError(response);
             }
 
-            return await SerializeResponse<UserLoginResponse>(response);
+            return await DeserializeResponse<UserLoginResponse>(response);
         }
 
         private async Task<UserLoginResponse> ReturnResponseError(HttpResponseMessage response)
         {
             return new UserLoginResponse
             {
-                ResponseResult = await SerializeResponse<ResponseResult>(response)
+                ResponseResult = await DeserializeResponse<ResponseResult>(response)
             };
         }
     }
