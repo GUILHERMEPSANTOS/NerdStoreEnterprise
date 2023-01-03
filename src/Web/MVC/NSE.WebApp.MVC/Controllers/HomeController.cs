74e6d8d0aeaaf065ccf734c0c9939dbe8a8c7d30
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-03 17:43:31 -0300

    feat: lidando com os erros trazidos pelo status

--- src/Web/MVC/NSE.WebApp.MVC/Controllers/HomeController.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace NSE.WebApp.MVC.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
@@ -23,9 +16,54 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [Route("error/{id:length(3,3)}")]
+    public IActionResult Error(int id)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        var modelError = GenerateInstanceErrorViewModel(id);
+
+        if (id == 500)
+        {
+            HandleErrorViewModelToInternalServerError(modelError);
+        }
+        else if (id == 404)
+        {
+            HandleErrorViewModelToNotFoundError(modelError);
+        }
+        else if (id == 403)
+        {
+            HandleErrorViewModelToForbiddenError(modelError);
+        }
+        else
+        {
+            return StatusCode(404);
+        }
+
+        return View("Error", modelError);
     }
+    private ErrorViewModel GenerateInstanceErrorViewModel(int id)
+    {
+        var modelError = new ErrorViewModel();
+
+        modelError.ErrorCode = id;
+
+        return modelError;
+    }
+
+    private void HandleErrorViewModelToInternalServerError(ErrorViewModel modelError)
+    {
+        modelError.Message = "Ocorreu um erro! Tente novamente mais tarde ou contate nosso suporte.";
+        modelError.Title = "Ocorreu um erro!";
+    }
+
+    private void HandleErrorViewModelToNotFoundError(ErrorViewModel modelError)
+    {
+        modelError.Message = "A página que está procurando não existe! <br /> Em caso de dúvidas entre em contato com nosso suporte.";
+        modelError.Title = "Ops! Página não encontrada.";
+    }
+    private void HandleErrorViewModelToForbiddenError(ErrorViewModel modelError)
+    {
+        modelError.Message = "Você não tem permissão para fazer isto.";
+        modelError.Title = "Acesso Negado!.";
+    }
+
 }
