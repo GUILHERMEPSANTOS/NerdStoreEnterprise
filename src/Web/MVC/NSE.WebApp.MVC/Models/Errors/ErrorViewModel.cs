4130f9223963dbe088953f67ff3d20ca92b88f96
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-03 17:47:34 -0300

    refactor: melhoria da model de erros

--- src/Web/MVC/NSE.WebApp.MVC/Models/Errors/ErrorViewModel.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Models/Errors/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace NSE.WebApp.MVC.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public int ErrorCode { get; set; }
+    public string Title { get; set; }
+    public string Message { get; set; }
 }
