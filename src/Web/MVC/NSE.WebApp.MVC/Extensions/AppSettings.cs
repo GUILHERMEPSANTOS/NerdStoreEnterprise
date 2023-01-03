3cc3e17cc0e77f910c6fe0a9aac4e93dc711b3fc
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-03 17:44:48 -0300

    feat: classe de configuração das apis externas

---
+++ src/Web/MVC/NSE.WebApp.MVC/Extensions/AppSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class AppSettings
+    {
+        public string AuthenticationUrl { get; set; }
+        public string CatalogoUrl { get; set; }
+    }
+}
