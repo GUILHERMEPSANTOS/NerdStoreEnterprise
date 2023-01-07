d84d965b85bba69d58552ec9b9473aaff6382dd2
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-07 14:47:48 -0300

    feat: implentação do repositorio

---
+++ src/Services/Catalogo/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Data;
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Domain.Entities;
+using NSE.Catalogo.API.Domain.Interfaces;
+
+namespace NSE.Catalogo.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+
+        private readonly CatalogoContext _catalogoContext;
+
+        public IUnitOfWork UnitOfWork => _catalogoContext;
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+
+        public async Task<IEnumerable<Produto>> GetAll()
+        {
+            return await _catalogoContext.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Produto> GetById(Guid id)
+        {
+            return await _catalogoContext.Produtos.FindAsync(id);
+        }
+        public void Add(Produto produto)
+        {
+            _catalogoContext.Produtos.Add(produto);
+        }
+
+        public async void Update(Produto produto)
+        {
+            _catalogoContext.Update(produto);
+        }
+
+        public void Dispose()
+        {
+            _catalogoContext?.Dispose();
+        }
+    }
+}
