a503641f41c48058b0cf8d9eb4e44e9d25c85a76
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-07 14:46:31 -0300

    refactor: correção dos diretorios

--- src/Building Blocks/Core/DomainObjects/Interfaces/IAggregateRoot.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Core.DomainObjects.Interfaces
-{
-    public interface IAggregateRoot { }
-}
--- src/Building Blocks/Core/DomainObjects/Interfaces/IRepository.cs
+++
@@ -1,7 +0,0 @@
-namespace Core.DomainObjects.Interfaces
-{
-    public interface IRepository<T> : IDisposable where T : IAggregateRoot
-    {
-        
-    }
-}
