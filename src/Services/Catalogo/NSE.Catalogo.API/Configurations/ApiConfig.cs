082dcd6ab75456ed43b88470c40aabb722fefa05
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-07 21:20:13 -0300

    feat: api configuration

---
+++ src/Services/Catalogo/NSE.Catalogo.API/Configurations/ApiConfig.cs
@@ -0,0 +1,37 @@
+namespace NSE.Catalogo.API.Configurations
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration Configuration)
+        {
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerConfiguration();
+            services.AddCorsConfiguration();
+            services.AddDbContextConfiguration(Configuration);
+
+            return services;
+        }
+
+        public static WebApplication UseApiConfiguration(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwaggerConfiguration();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseRouting();
+
+            app.UseCorsConfiguration();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
