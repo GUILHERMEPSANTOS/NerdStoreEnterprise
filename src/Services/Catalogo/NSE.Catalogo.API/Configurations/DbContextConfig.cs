88c27c3648fccecb24e9fdf6f226297711a80771
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-07 21:20:44 -0300

    feat: config contexto do database

---
+++ src/Services/Catalogo/NSE.Catalogo.API/Configurations/DbContextConfig.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Data;
+
+namespace NSE.Catalogo.API.Configurations
+{
+    public static class DbContextConfig
+    {
+        public static IServiceCollection AddDbContextConfiguration(this IServiceCollection services, IConfiguration Configuration)
+        {
+
+            services.AddDbContext<CatalogoContext>(options =>
+                  options
+                       .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                       .LogTo(Console.WriteLine)
+                       .EnableSensitiveDataLogging());
+
+            return services;
+        }
+    }
+}
