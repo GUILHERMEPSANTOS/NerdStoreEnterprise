ce374e3945a2b285605096a4f336b6e91d4d7573
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-09 13:40:17 -0300

    refactor: mudança do nome da variavel builder para services

--- src/Services/Identidade/NSE.Identidade.API/Configurations/IdentityConfig.cs
+++ src/Services/Identidade/NSE.Identidade.API/Configurations/IdentityConfig.cs
@@ -5,23 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NSE.Identidade.API.Data;
+using NSE.WebApi.Core.Identidade;
 
 namespace NSE.Identidade.API.Configurations
 {
     public static class IdentityConfig
     {
-        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection builder, IConfiguration configuration)
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration Configuration)
         {
-            builder.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options
+                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
 
-            builder.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            return builder;
+            services.AddJwtConfiguration(Configuration);
+
+            return services;
         }
     }
 }
