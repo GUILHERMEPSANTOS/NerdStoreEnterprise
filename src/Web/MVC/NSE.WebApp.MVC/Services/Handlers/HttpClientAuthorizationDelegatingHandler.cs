7178d5cfda717267110508b793910885fe9d9d6b
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-16 20:16:15 -0300

    feat: add delegatingHandler

---
+++ src/Web/MVC/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,34 @@
+using System.Net.Http.Headers;
+using NSE.WebApp.MVC.Extensions;
+
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authenticationHeader = _user.GetHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authenticationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authenticationHeader });
+            }
+
+            var token = _user.GetUserToken();
+
+            if (token is not null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
