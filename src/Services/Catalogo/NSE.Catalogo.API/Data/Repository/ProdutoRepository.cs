859c2c98c5843739eeb7cf04c3effde2e588e58b
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-19 15:12:25 -0300

    refactor: removendo namespaces desnecessários

--- src/Services/Catalogo/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
+++ src/Services/Catalogo/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Core.Data;
 using Microsoft.EntityFrameworkCore;
 using NSE.Catalogo.API.Domain.Entities;
