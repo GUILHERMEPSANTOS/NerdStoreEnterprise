4af550eb26ee22d65da03bd3b8a45ddf4bde1870
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-20 18:31:26 -0300

    feat: criação da classe newCustomerCommand implementando Command

---
+++ src/Services/Cliente/NSE.Cliente.API/Application/Customer/Commands/NewCustomerCommand.cs
@@ -0,0 +1,21 @@
+using Core.Messages;
+
+namespace NSE.Cliente.API.Application.Commands
+{
+    public class NewCustomerCommand: Command
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public NewCustomerCommand(Guid id, string name, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
