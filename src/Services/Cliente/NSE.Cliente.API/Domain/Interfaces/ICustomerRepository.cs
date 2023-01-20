19b349203765481b7e67cc61367225a3cbcbc886
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-20 20:30:11 -0300

    feat: interface do repositorio customer

---
+++ src/Services/Cliente/NSE.Cliente.API/Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+using Core.DomainObjects.Data;
+using NSE.Cliente.API.Domain.Entities;
+
+namespace NSE.Cliente.API.Domain.Interfaces
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        void Add(Customer customer);
+        Task<IEnumerable<Customer>> GetAll();
+        Task<Customer> GetByCpf(string cpf);
+    }
+}
