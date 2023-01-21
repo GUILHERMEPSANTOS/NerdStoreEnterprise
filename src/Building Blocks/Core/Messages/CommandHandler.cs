a83cd1ab1d16f391bac467d56a6942973f3d956b
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-20 21:52:55 -0300

    feat: criação do metodo de commit no CommandHandler

--- src/Building Blocks/Core/Messages/CommandHandler.cs
+++ src/Building Blocks/Core/Messages/CommandHandler.cs
@@ -1,3 +1,4 @@
+using Core.Data;
 using FluentValidation.Results;
 
 namespace Core.Messages
@@ -14,6 +15,13 @@ namespace Core.Messages
         protected void AddError(string message)
         {
             ValidationResult.Errors.Add(new ValidationFailure(string.Empty, message));
+        }
+
+        protected async Task<ValidationResult> PersistData(IUnitOfWork uow)
+        {
+            if (!await uow.Commit()) AddError("Houve um erro ao persistir os dados");
+
+            return ValidationResult;
         }
     }
 }
