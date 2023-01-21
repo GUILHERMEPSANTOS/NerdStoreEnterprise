cc807d1161b574e91f666cf971664b1fbee7ae46
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-21 13:26:57 -0300

    feat: implementação da lista de eventos

--- src/Building Blocks/Core/DomainObjects/Entity.cs
+++ src/Building Blocks/Core/DomainObjects/Entity.cs
@@ -1,14 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Core.Messages;
 
 namespace Core.DomainObjects
 {
     public abstract class Entity
     {
         public Guid Id { get; set; }
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        private List<Event> _events;
+        public IReadOnlyCollection<Event> Notification => _events?.AsReadOnly();
 
+        public void AddEvent(Event @event)
+        {
+            _events ??= new List<Event>();
+            _events.Add(@event);    
+        }
+        public void RemoveEvent(Event _eventItem)
+        {
+            _events?.Remove(_eventItem);
+        }
+
+        public void ClearEvents()
+        {
+            _events?.Clear();
+        }
+
+        #region Comparisons
         public override bool Equals(object? obj)
         {
             var compareTo = obj as Entity;
@@ -44,5 +63,6 @@ namespace Core.DomainObjects
         {
             return $"{GetType().Name} [Id={Id}]";
         }
+        #endregion
     }
 }
