832c8f87163968d301b9fb1e638feae2ca9a3f10
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-21 13:27:08 -0300

    feat: Event class base

--- src/Building Blocks/Core/Messages/Event.cs
+++ src/Building Blocks/Core/Messages/Event.cs
@@ -1,7 +1,13 @@
+using MediatR;
+
 namespace Core.Messages
 {
-    public class Event : Message
+    public class Event : Message, INotification
     {
-        
+        public DateTime Timestamp { get; private set; }
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
     }
 }
