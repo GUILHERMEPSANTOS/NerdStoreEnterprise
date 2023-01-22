15c8198996fdff7f6a8c4c468206a8767d670bf4
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-22 12:35:21 -0300

    feat: customer controller test

--- src/Services/Cliente/NSE.Cliente.API/Controllers/CustomerController.cs
+++ src/Services/Cliente/NSE.Cliente.API/Controllers/CustomerController.cs
@@ -1,7 +1,39 @@
+
+using Microsoft.AspNetCore.Mvc;
+using NSE.Cliente.API.Application.DTO;
+using NSE.Cliente.API.Application.Interfaces;
+using NSE.WebApi.Core.Controllers;
+
 namespace NSE.Cliente.API.Controllers
 {
-    public class CustomerController
+    [Route("customers")]
+    public class CustomerController : MainController
     {
-        
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+
+            var newCustomerDTO = new NewCustomerDTO();
+
+            newCustomerDTO.Cpf = "53225780057";
+
+            newCustomerDTO.Email = "thiagopereiradossantos41@outlook.com";
+
+            newCustomerDTO.Id = Guid.NewGuid();
+
+            newCustomerDTO.Name = "Thiago";
+
+            var result = await _customerService.Add(newCustomerDTO);
+
+            return CustomResponse(result);
+        }
+
     }
 }
