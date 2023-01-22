82fe991917354c2ae67bcdf8c9bea942f78d5a5b
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-22 12:33:13 -0300

    feat: implementação/interface do service

--- src/Services/Cliente/NSE.Cliente.API/Application/Interfaces/ICustomerService.cs
+++ src/Services/Cliente/NSE.Cliente.API/Application/Interfaces/ICustomerService.cs
@@ -1,7 +1,10 @@
+using FluentValidation.Results;
+using NSE.Cliente.API.Application.DTO;
+
 namespace NSE.Cliente.API.Application.Interfaces
 {
     public interface ICustomerService
     {
-        
+        Task<ValidationResult> Add(NewCustomerDTO newCustomerDTO);
     }
 }
--- src/Services/Cliente/NSE.Cliente.API/Application/Service/CustomerService.cs
+++ src/Services/Cliente/NSE.Cliente.API/Application/Service/CustomerService.cs
@@ -1,10 +1,28 @@
 using AutoMapper;
+using Core.Mediator;
+using FluentValidation.Results;
+using NSE.Cliente.API.Application.Customer.Commands;
+using NSE.Cliente.API.Application.DTO;
 using NSE.Cliente.API.Application.Interfaces;
 
 namespace NSE.Cliente.API.Application.Service
 {
     public class CustomerService : ICustomerService
     {
-         private readonly IMapper _mapper;
+        private readonly IMapper _mapper;
+        private readonly IMediatorHandler _mediator;
+
+        public CustomerService(IMapper mapper, IMediatorHandler mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public async Task<ValidationResult> Add(NewCustomerDTO newCustomerDTO)
+        {
+            var newCustomerCommand = _mapper.Map<NewCustomerCommand>(newCustomerDTO);
+
+            return await _mediator.SendCommand<NewCustomerCommand>(newCustomerCommand);
+        }
     }
 }
