436f35b35b77a3565be8929cb0dc1c14c2eb2a77
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-24 21:40:29 -0300

    feat: mensagem de retorno dos events

---
+++ src/Building Blocks/Core/Messages/Integration/ResponseMessage.cs
@@ -0,0 +1,14 @@
+using FluentValidation.Results;
+
+namespace Core.Messages.Integration
+{
+    public class ResponseMessage : Message
+    {
+        public ValidationResult ValidationResult { get; set; }
+
+        public ResponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+    }
+}
