982f0c78d55154cdce8113166b1031faac8f4b8c
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-25 15:52:33 -0300

    feat: add addMessageBuss dependency injection

---
+++ src/Building Blocks/MessageBus/NSE.MessageBus/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.MessageBus
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddMessageBus(this IServiceCollection services, string connection)
+        {
+            if (string.IsNullOrEmpty(connection)) throw new ArgumentNullException();
+
+            services.AddSingleton<IMessageBus>(new MessageBus(connection));
+
+            return services;
+        }
+    }
+}
