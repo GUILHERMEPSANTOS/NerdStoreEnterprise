4b2a5aac7c097a27690e1a2db2f47ba8502fbc77
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-25 13:49:52 -0300

    refactor: adicionando espaços em branco

--- src/Building Blocks/MessageBus/NSE.MessageBus/IMessageBus.cs
+++ src/Building Blocks/MessageBus/NSE.MessageBus/IMessageBus.cs
@@ -7,14 +7,21 @@ namespace NSE.MessageBus
     public interface IMessageBus  : IDisposable
     {
         bool IsConnected { get; }
+
         IAdvancedBus AdvancedBus { get; }
+
         void Publish<T>(T message) where T : IntegrationEvent;
+
         Task PublishAsync<T>(T message) where T : IntegrationEvent;
+
         void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+
         AwaitableDisposable<SubscriptionResult> SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+
         TResponse Request<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage;
+
         Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage;
@@ -22,6 +29,7 @@ namespace NSE.MessageBus
         IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage;
+            
         AwaitableDisposable<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage;
