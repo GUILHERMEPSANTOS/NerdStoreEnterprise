6f3f003de334ad6ce67120fc0bdc6bfa6be6a22d
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-24 21:40:44 -0300

    feat: event user registered

---
+++ src/Building Blocks/Core/Messages/Integration/UserRegisteredIntegrationEvent.cs
@@ -0,0 +1,18 @@
+namespace Core.Messages.Integration
+{
+    public class UserRegisteredIntegrationEvent : IntegrationEvent  
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public UserRegisteredIntegrationEvent(Guid id, string name, string email, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
