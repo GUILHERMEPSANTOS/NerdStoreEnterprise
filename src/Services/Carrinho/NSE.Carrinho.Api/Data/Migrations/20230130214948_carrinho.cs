de7930523758e70c3627c41f94a6c4e8318e6a4c
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-30 20:04:12 -0300

    feat: migrations config

---
+++ src/Services/Carrinho/NSE.Carrinho.Api/Data/Migrations/20230130214948_carrinho.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NSE.Carrinho.API.Data.Migrations
+{
+    public partial class carrinho : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
