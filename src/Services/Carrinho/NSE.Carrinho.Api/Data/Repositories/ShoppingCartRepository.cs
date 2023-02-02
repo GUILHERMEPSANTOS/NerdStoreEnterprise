fab034a9c3b7e31c3b043a093ce385339fe331e7
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-01 21:08:32 -0300

    feat: implementação do repositorio

---
+++ src/Services/Carrinho/NSE.Carrinho.Api/Data/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,46 @@
+using Core.Data;
+using Microsoft.EntityFrameworkCore;
+using NSE.Carrinho.Api.Domain;
+using NSE.Carrinho.Api.Domain.Interfaces;
+
+namespace NSE.Carrinho.Api.Data.Repositories
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private readonly ShoppingCartContext _shoppingCartContext;
+        public IUnitOfWork UnitOfWork => _shoppingCartContext;
+
+        public ShoppingCartRepository(ShoppingCartContext shoppingCartContext)
+        {
+            _shoppingCartContext = shoppingCartContext;
+        }
+
+        public async Task<CustomerShoppingCart> GetCustomerShoppingCart(Guid customerId)
+        {
+            return await _shoppingCartContext.CustomerShoppingCarts
+                .Include(shopCart => shopCart.Items)
+                .AsSplitQuery()
+                .FirstOrDefaultAsync(shopCart => shopCart.CustomerId == customerId);
+        }
+
+        public void AddCustomerShoppingCart(CustomerShoppingCart customerShoppingCart)
+        {
+            _shoppingCartContext.CustomerShoppingCarts.Add(customerShoppingCart);
+        }
+
+        public void UpdateCustomerShoppingCart(CustomerShoppingCart customerShoppingCart)
+        {
+            _shoppingCartContext.CustomerShoppingCarts.Update(customerShoppingCart);
+        }
+
+        public void AddCartItem(CartItem cartItem)
+        {
+            _shoppingCartContext.CartItems.Add(cartItem);
+        }
+
+        public void UpdateCartItem(CartItem cartItem)
+        {
+            _shoppingCartContext.CartItems.Update(cartItem);
+        }
+    }
+}
