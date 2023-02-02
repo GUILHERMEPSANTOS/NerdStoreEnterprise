3756fbd3f3be4a3c6d10ce00c59a94651b8a251e
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-01 21:07:37 -0300

    feat: injeção de dependencia

--- src/Services/Carrinho/NSE.Carrinho.Api/Configurations/DependencyInjectionConfig.cs
+++ src/Services/Carrinho/NSE.Carrinho.Api/Configurations/DependencyInjectionConfig.cs
@@ -1,3 +1,7 @@
+using NSE.Carrinho.Api.Application.Interfaces;
+using NSE.Carrinho.Api.Application.Services;
+using NSE.Carrinho.Api.Data.Repositories;
+using NSE.Carrinho.Api.Domain.Interfaces;
 using NSE.WebApi.Core.User;
 
 namespace NSE.Carrinho.Api.Configurations
@@ -8,6 +12,9 @@ namespace NSE.Carrinho.Api.Configurations
         {
             services.AddScoped<IAspNetUser, AspNetUser>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+            services.AddScoped<IShoppingCartService, ShoppingCartService>();
+            services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
 
             return services;
         }
