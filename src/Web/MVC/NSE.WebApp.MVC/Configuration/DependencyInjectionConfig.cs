29deac418f97fa1a243b53314721cfb379b646e0
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-11 17:27:31 -0300

    feat: configuração da injeção  de dependencia do client

--- src/Web/MVC/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -6,6 +6,7 @@ using NSE.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using static NSE.WebApp.MVC.Extensions.CpfAttributeAdapter;
 using NSE.WebApi.Core.User;
+using NSE.WebApp.MVC.Services.Interfaces;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -13,18 +14,31 @@ namespace NSE.WebApp.MVC.Configuration
     {
         public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
-            services.AddHttpClient<IAuthenticationService, AuthenticationService>();
             services.AddSingleton<IValidationAttributeAdapterProvider, CpfValidationAttributeAdapterProvider>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
+
+            #region  HttpServices
+
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
+            services.AddHttpClient<IAuthenticationService, AuthenticationService>()
+                .AddPolicyHandler(PollyExtensions.WaitAndTry())
+                .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
             services
                 .AddHttpClient<ICatalogoService, CatalogoService>()
                 .AddPolicyHandler(PollyExtensions.WaitAndTry())
                 .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)))
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+            services
+                .AddHttpClient<ICarrinhoService, CarrinhoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddPolicyHandler(PollyExtensions.WaitAndTry())
+                .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             return services;
+
+            #endregion
         }
     }
 }
