86201712313a391532c377884182a97803db51d8
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-23 21:18:34 -0300

    fiz: correção propriedades não obrigatorias

--- src/api-gateways/Compras/NSE.Bff.Compras/DTOs/CartItemDTO.cs
+++ src/api-gateways/Compras/NSE.Bff.Compras/DTOs/CartItemDTO.cs
@@ -3,9 +3,9 @@ namespace NSE.Bff.Compras.DTOs
     public class CartItemDTO
     {
         public Guid ProductId { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
-        public string Image { get; set; }
+        public decimal? Price { get; set; }
+        public string? Image { get; set; }
     }
 }
