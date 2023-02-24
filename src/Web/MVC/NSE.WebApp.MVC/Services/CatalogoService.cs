81f7300f94cac8d975537b3a100b922b6c6fdf72
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-23 21:23:08 -0300

    refactor: remoção de namespaces nunca usados

--- src/Web/MVC/NSE.WebApp.MVC/Services/CatalogoService.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Interfaces;
