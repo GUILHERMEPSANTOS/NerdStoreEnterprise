8dabf676f8d8e8f509fdbf7cc98cb535b9b91766
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-23 21:21:51 -0300

    refactor: troca da api para o bff

--- src/Web/MVC/NSE.WebApp.MVC/Extensions/AppSettings.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Extensions/AppSettings.cs
@@ -4,6 +4,6 @@ namespace NSE.WebApp.MVC.Extensions
     {
         public string AuthenticationUrl { get; set; }
         public string CatalogoUrl { get; set; }
-        public string CarrinhoUrl { get; set; }
+        public string ComprasBffUrl { get; set; }
     }
 }
