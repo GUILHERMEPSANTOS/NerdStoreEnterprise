3b3a374edfc4025cf82ff54e606c574fd53b18f2
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-02-23 21:20:10 -0300

    refactor: add bff dependency injection

--- src/Web/MVC/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using static NSE.WebApp.MVC.Extensions.CpfAttributeAdapter;
 using NSE.WebApi.Core.User;
 using NSE.WebApp.MVC.Services.Interfaces;
+using NSE.WebApi.Core.Extensions;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -31,7 +32,7 @@ namespace NSE.WebApp.MVC.Configuration
                 .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)))
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
             services
-                .AddHttpClient<ICarrinhoService, CarrinhoService>()
+                .AddHttpClient<IComprasBffService, ComprasBffService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.WaitAndTry())
                 .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
