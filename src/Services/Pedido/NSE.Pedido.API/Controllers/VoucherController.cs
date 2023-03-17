99e279ddeff319b8d0fb054cfab24945494276aa
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-03-17 20:44:12 -0300

    feat: create voucher controller

---
+++ src/Services/Pedido/NSE.Pedido.API/Controllers/VoucherController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+using NSE.WebApi.Core.Controllers;
+
+namespace NSE.Pedido.API.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+        
+    }
+}
