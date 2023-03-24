052b19debdc224a1a63660c553a939adc14413a5
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-03-24 19:30:56 -0300

    feat: add dependency injection

--- src/Services/Pedido/NSE.Pedido.API/Configurations/DependencyInjectionConfig.cs
+++ src/Services/Pedido/NSE.Pedido.API/Configurations/DependencyInjectionConfig.cs
@@ -1,6 +1,10 @@
 
 using Core.Mediator;
 using MediatR;
+using NSE.Pedido.API.Application.Queries;
+using NSE.Pedido.Domain.Vouchers;
+using NSE.Pedido.Infra.Repositories;
+using NSE.WebApi.Core.User;
 
 namespace NSE.Pedido.API.Configurations
 {
@@ -8,8 +12,15 @@ namespace NSE.Pedido.API.Configurations
     {
         public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
         {
+
+            services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser,   AspNetUser>();
+
             services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
 
             return services;
         }
