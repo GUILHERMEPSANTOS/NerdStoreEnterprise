0de584d07c2317ff6cb21e2c7c969d71e43549b1
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-03-24 19:31:13 -0300

    feat: implementação do metodo GetVoucherByCode

--- src/Services/Pedido/NSE.Pedido.API/Controllers/VoucherController.cs
+++ src/Services/Pedido/NSE.Pedido.API/Controllers/VoucherController.cs
@@ -1,4 +1,8 @@
+using System.Net;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Pedido.API.Application.DTO;
+using NSE.Pedido.API.Application.Queries;
 using NSE.WebApi.Core.Controllers;
 
 namespace NSE.Pedido.API.Controllers
@@ -6,6 +10,25 @@ namespace NSE.Pedido.API.Controllers
     [Authorize]
     public class VoucherController : MainController
     {
-        
+        private readonly IVoucherQueries _voucherQueries;
+
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
+
+        [HttpGet("voucher/{code}")]
+        [ProducesResponseType(typeof(VoucherDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetVoucherByCode(string code)
+        {
+            if (string.IsNullOrEmpty(code)) return NotFound();
+
+            var voucherDTO = await _voucherQueries.GetVoucherByCode(code);
+
+            if (voucherDTO is null) return NotFound();
+
+            return CustomResponse(voucherDTO);
+        }
     }
 }
