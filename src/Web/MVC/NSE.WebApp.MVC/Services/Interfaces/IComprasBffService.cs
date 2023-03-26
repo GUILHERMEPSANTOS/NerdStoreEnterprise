700e755cfd2ad683a5a1f3c736fb1899ddd5c4a2
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-03-26 15:43:09 -0300

    refactor: correção do do nome do arquivo

--- src/Web/MVC/NSE.WebApp.MVC/Services/Interfaces/IComprasBffService.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Services/Interfaces/IComprasBffService.cs
@@ -1,11 +1,13 @@
 using Core.Communication;
 using NSE.WebApp.MVC.Models.Carrinho;
+using NSE.WebApp.MVC.Models.Pedido;
 
 namespace NSE.WebApp.MVC.Services.Interfaces
 {
     public interface IComprasBffService
     {
         Task<ShoppingCartViewModel> GetShoppingCart();
+        Task<ResponseResult> ApplyVoucher(string code);
         Task<int> GetShoppingCartItemsQuantity();
         Task<ResponseResult> AddCartItem(CartItemViewModel item);
         Task<ResponseResult> UpdateCartItem(Guid productId, CartItemViewModel item);
