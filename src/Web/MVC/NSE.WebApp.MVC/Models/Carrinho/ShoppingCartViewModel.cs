17f673350ecd24e7210fae0b44419399d9863aab
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-03-26 15:42:10 -0300

    add: class voucherViewModel and  properties in ShoppingCartViewModel

--- src/Web/MVC/NSE.WebApp.MVC/Models/Carrinho/ShoppingCartViewModel.cs
+++ src/Web/MVC/NSE.WebApp.MVC/Models/Carrinho/ShoppingCartViewModel.cs
@@ -1,8 +1,13 @@
+using NSE.WebApp.MVC.Models.Pedido;
+
 namespace NSE.WebApp.MVC.Models.Carrinho
 {
     public class ShoppingCartViewModel
     {
         public decimal Total { get; set; }
+        public VoucherViewModel Voucher { get; set; }
+        public bool HasVoucher { get; set; }
+        public decimal Discount { get; set; }
         public List<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
     }
 }
---
+++ src/Web/MVC/NSE.WebApp.MVC/Models/Pedido/VoucherViewModel.cs
@@ -0,0 +1,7 @@
+namespace NSE.WebApp.MVC.Models.Pedido
+{
+    public class VoucherViewModel
+    {
+        public  string Code { get; set; }
+    }
+}
