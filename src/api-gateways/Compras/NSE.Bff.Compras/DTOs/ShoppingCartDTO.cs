b33f6b4ea4b750813261372a3c3965a6a994a873
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-03-26 15:38:08 -0300

    feat: adicionando propriedades ao ShoppingCartDTO

--- src/api-gateways/Compras/NSE.Bff.Compras/DTOs/ShoppingCartDTO.cs
+++ src/api-gateways/Compras/NSE.Bff.Compras/DTOs/ShoppingCartDTO.cs
@@ -4,6 +4,8 @@ namespace NSE.Bff.Compras.DTOs
     {
         public decimal Total { get; set; }
         public decimal Discount { get; set; }
+        public bool HasVoucher { get; set; }
+        public VoucherDTO Voucher { get; set; }
         public List<CartItemDTO> Items { get; set; } = new List<CartItemDTO>();
     }
 }
