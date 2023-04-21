f8db1c8981ee8548b158ae22135fa87e6fba9ca1
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-04-21 17:19:39 -0300

    feat: objeto de validação

---
+++ src/Services/Pedido/NSE.Pedido.API/Application/Validations/AddOrderValidation.cs
@@ -0,0 +1,40 @@
+using FluentValidation;
+using NSE.Pedido.API.Application.Commands;
+
+namespace NSE.Pedido.API.Application.Validations
+{
+    public class AddOrderValidation : AbstractValidator<AddOrderCommand>
+    {
+        public AddOrderValidation()
+        {
+            RuleFor(addOrder => addOrder.CustomerId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(addOrder => addOrder.OrderItems.Count)
+                .GreaterThan(0)
+                .WithMessage("O pedido precisa ter no minímo 1 item");
+
+            RuleFor(addOrder => addOrder.Amount)
+                .GreaterThan(0)
+                .WithMessage("Valor do pedido inválido");
+
+            RuleFor(addOrder => addOrder.CardNumber)
+                .CreditCard()
+                .WithMessage("Número do cartão inválido");
+
+            RuleFor(addOrder => addOrder.Holder)
+                .NotEmpty()
+                .WithMessage("Nome do portador do cartão requerido.");
+
+            RuleFor(addOrder => addOrder.SecurityCode.Length)
+                .GreaterThan(2)
+                .LessThan(5)
+                .WithMessage("O CVV do cartão precisa ter 3 ou 4 números");
+
+            RuleFor(addOrder => addOrder.ExpirationDate)
+                .NotEmpty()
+                .WithMessage("Data de expiração do cartão  requerida");
+        }
+    }
+}
