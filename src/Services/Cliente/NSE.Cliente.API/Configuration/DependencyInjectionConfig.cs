3fe8c33cdc5ed9aa2048c7cf2a13b59547430cd4
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-04-29 14:42:06 -0300

    feat: add dependency injection

--- src/Services/Cliente/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
+++ src/Services/Cliente/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -5,6 +5,8 @@ using NSE.Cliente.API.Application.Mappings;
 using NSE.Cliente.API.Application.Service;
 using NSE.Cliente.API.Data.Repositories;
 using NSE.Cliente.API.Domain.Interfaces;
+using NSE.WebApi.Core.User;
+using NSE.Cliente.API.Application.Customer.Queries;
 
 namespace NSE.Cliente.API.Configuration
 {
@@ -12,12 +14,30 @@ namespace NSE.Cliente.API.Configuration
     {
         public static IServiceCollection AddDependencyInjectionConfiguration(this IServiceCollection services)
         {
+            #region Http Accessor
+            services.AddScoped<IAspNetUser, AspNetUser>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            #endregion
+
+            #region AutoMapper
             services.AddAutoMapper(typeof(DTOToCommandMappingProfile));
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+            #endregion
+
+            #region Mediator
             services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<IMediatorHandler, MediatorHandler>();
+            #endregion
+
+            #region Services/Repositories 
             services.AddScoped<ICustomerService, CustomerService>();
             services.AddScoped<ICustomerRepository, CustomerRepository>();
+            #endregion
 
+            #region Queries 
+            services.AddScoped<ICustomerAddressQuery, CustomerAddressQuery>();
+            #endregion
+            
             return services;
         }
     }
