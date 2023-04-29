161906537bdb7beab7c34fcc2367215ff8544001
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-04-29 14:40:27 -0300

    feat: add address validation

---
+++ src/Services/Cliente/NSE.Cliente.API/Application/Customer/Validations/AddAddressValidation.cs
@@ -0,0 +1,35 @@
+using FluentValidation;
+using NSE.Cliente.API.Application.Customer.Commands;
+
+namespace NSE.Cliente.API.Application.Customer.Validations
+{
+    public class AddAddressValidation : AbstractValidator<AddAddressCommand>
+    {
+        public AddAddressValidation()
+        {
+            RuleFor(c => c.Street)
+                   .NotEmpty()
+                   .WithMessage("informe o logradouro");
+
+            RuleFor(c => c.HouseNumber)
+                .NotEmpty()
+                .WithMessage("Informe o número");
+
+            RuleFor(c => c.ZipCode)
+                .NotEmpty()
+                .WithMessage("Informe o CEP");
+
+            RuleFor(c => c.Neighborhood)
+                .NotEmpty()
+                .WithMessage("Informe o bairro");
+
+            RuleFor(c => c.City)
+                .NotEmpty()
+                .WithMessage("Informe a cidade");
+
+            RuleFor(c => c.State)
+                .NotEmpty()
+                .WithMessage("Informe o Estado");
+        }
+    }
+}
